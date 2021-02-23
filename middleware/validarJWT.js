088d8332f4feb7b3e72ee5a1d9821c5704669579
duvan <duvanli@hotmail.es>
2021-02-23 17:57:45 -0500

    backend acabado

--- middleware/validarJWT.js
+++ middleware/validarJWT.js
@@ -1,42 +1,40 @@
 const {response} = require('express');
 const jwt= require('jsonwebtoken')
 
-const validarJWT = ( req, res = response, next ) => {
+const validarJWT =( req,res=response,next ) => {
 
-    // x-token headers
-    const token = req.header('x-token');
+        const token = req.header('x-token')
 
-    if ( !token ) {
-        return res.status(401).json({
-            ok: false,
-            msg: 'No hay token en la petición'
-        });
-    }
-
-    try {
-        
-        const { uid, name } = jwt.verify(
-            token,
-            process.env.Secret_JWT
-        );
-
-        req.uid = uid;
-        req.name = name;
+        if(!token){
+            return res.status(401).json({
+                ok:false,
+                msg: 'no hay token en la peticion'
+            })
+        }
 
 
-    } catch (error) {
-        return res.status(401).json({
-            ok: false,
-            msg: 'Token no válido'
-        });
-    }
+        try {
+            
+            const {uid,name} =jwt.verify(
+                token,
+                process.env.Secret_JWT
+            )
+
+            req.uid=uid;
+            req.name= name;
+
+
+        } catch (error) {
+            return res.status(401).json({
+                ok:false,
+                msg: 'token no valido'
+            })
+        }
 
 
 
-    next();
+         next();
 }
 
 
-module.exports = {
-    validarJWT
-}
+module.exports = { validarJWT}
